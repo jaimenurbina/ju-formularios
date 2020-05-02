d8488b3cd7bbb6700bf788b1b99bf81547c83b58
jaimenurbina <jaimen_urbina@hotmail.com>
2020-05-02 19:17:12 -0400

    Primera Actualizacion

---
+++ ju-formularios.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: ju Formularios Personalizados
+Plugin URI: https://fullsystem.com.ve
+Description: Agregar Formularios Personalizados a traves del shortcode [ju-formularios]
+Version: 1.0
+Requires at least: 5.4.1
+Tags: ju-formularios
+License: GPL V2
+Author: Jaimen Urbina <jaimen_urbina@hotmail.com>
+Author URI: https://fullsystem.com.ve
+Text Domain: ju-formularios
+Domain Path: /languages
+*/ 
